5af1efef4b9d2e1c2b68f4c9bb5a3bd1d48df9b3
Wercton <werctonmatheus@gmail.com>
2020-09-12 17:49:03 -0300

    user can now register

--- users/views.py
+++ users/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import FormularioRegistro
 
-def register(request):
-    form = UserCreationForm()
+def registrar(request):
+    if request.method == 'POST':
+        form = FormularioRegistro(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'{username}, willkommen! Ihr Konto wurde erfolgreich erstellt! :)')
+            return redirect('blog-home')
+    else:
+        form = FormularioRegistro()
     return render(request, 'users/register.html', {'form': form})
+
+# messages.debug
+# messages.info
+# messages.sucess
+# messages.warning
+# messages.error
